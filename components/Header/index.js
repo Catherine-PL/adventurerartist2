6a675295abf7f11fc1deabb38aae5cc7d8c9e7b0
Katarzyna Kosiak <kate2805@op.pl>
2023-07-29 22:10:25 +0200

    Add link to YT

--- components/Header/index.js
+++ components/Header/index.js
@@ -120,7 +120,7 @@ const Header = ({ handleWorkScroll, handleAboutScroll, isBlog }) => {
         </h1>
         {!isBlog ? (
           <div className="flex">
-            <Button onClick={handleWorkScroll}>Work</Button>
+            <Button onClick={handleWorkScroll}>Featured blog posts</Button>
             <Button onClick={handleAboutScroll}>About</Button>
             {showBlog && (
               <Button onClick={() => router.push("/blog")}>Blog</Button>
